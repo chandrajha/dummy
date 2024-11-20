1fa8105caa4ee17c35ba9c2fbd383612b7aa1a3e
Chandra Jha <jamresh91@gmail.com>
2024-11-20 07:18:56 +0530

    added test and test1 files

---
+++ test.ts
@@ -0,0 +1,6 @@
+class test{
+    dummyFunc(){
+        console.log("it is dummy code");
+        
+    }
+}
---
+++ test1.ts
@@ -0,0 +1,6 @@
+class test1{
+    dummyFunc1(){
+        console.log("it is dummy code for test1");
+        
+    }
+}
